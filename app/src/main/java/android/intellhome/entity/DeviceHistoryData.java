0dc8aae608f869a2e0dafbc3b7d2a22d40cd8cf4
Quentin <Quentin_L_0415@outlook.com>
2016-10-19 17:36:13 +0800

    added DeviceHistoryData class which is to be used for parsing and stroing incoming JSON data

---
+++ app/src/main/java/android/intellhome/entity/DeviceHistoryData.java
@@ -0,0 +1,17 @@
+package android.intellhome.entity;
+
+/**
+ * Created by Quentin on 19/10/2016.
+ */
+public class DeviceHistoryData {
+
+    public long id;
+    public long device_sn;
+    public String pdatetime;
+    public int device_state;
+    public int device_U;
+    public int device_I;
+    public int device_P;
+    public int device_electricity;
+    public String remark;
+}
