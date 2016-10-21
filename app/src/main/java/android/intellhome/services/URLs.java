b83ecdab8eeadf5995ff2e7c07301f1dfd2bf445
Quentin <Quentin_L_0415@outlook.com>
2016-10-21 21:31:25 +0800

    added currently available path and JSON to URLs.java

--- app/src/main/java/android/intellhome/services/URLs.java
+++ app/src/main/java/android/intellhome/services/URLs.java
@@ -6,4 +6,7 @@ package android.intellhome.services;
 public class URLs {
 
     public static final String BASE_URL = "http://192.168.1.135:3000/";
+
+    //http://192.168.1.135:3000/tools/gethisData/getHis?_hserverSN=0000000000000107&begtime=20161007104353&endtime=20161008004753
+    //{"result":[{"id":29472,"device_sn":"0001","updatetime":"2016-10-07T07:30:55.000Z","device_state":1,"device_U":220,"device_I":1,"divice_P":0,"device_electricity":8.9,"remark":null}]}
 }
