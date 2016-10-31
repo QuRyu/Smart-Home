518e1002f9ce05f9bcb61f6a05ddafec7e9f700b
Quentin <Quentin_L_0415@outlook.com>
2016-10-31 15:28:34 +0800

    finished the view of DeviceMonitorActivity

--- app/src/main/java/android/intellhome/DeviceMonitorActivity.java
+++ app/src/main/java/android/intellhome/DeviceMonitorActivity.java
@@ -1,7 +1,26 @@
 package android.intellhome;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.zcw.togglebutton.ToggleButton;
+
 /**
  * Created by Quentin on 31/10/2016.
  */
-public class DeviceMonitorActivity {
+public class DeviceMonitorActivity extends AppCompatActivity {
+
+    ToggleButton button;
+
+    TextView mTV_component_name;
+    TextView mTV_component_id;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        
+    }
 }
