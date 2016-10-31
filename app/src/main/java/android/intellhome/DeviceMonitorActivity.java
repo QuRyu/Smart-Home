af1ce140a9b8c280bae3c1a795836b16c5bb6c00
Quentin <Quentin_L_0415@outlook.com>
2016-10-31 16:17:06 +0800

    bound basic components in activity

--- app/src/main/java/android/intellhome/DeviceMonitorActivity.java
+++ app/src/main/java/android/intellhome/DeviceMonitorActivity.java
@@ -1,9 +1,13 @@
 package android.intellhome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zcw.togglebutton.ToggleButton;
 
@@ -12,15 +16,46 @@ import com.zcw.togglebutton.ToggleButton;
  */
 public class DeviceMonitorActivity extends AppCompatActivity {
 
+    private boolean toggleOn;
+
+    Button mBT_history;
+
     ToggleButton button;
 
     TextView mTV_component_name;
     TextView mTV_component_id;
 
+    DeviceMonitorController controller;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        
+        controller = new DeviceMonitorController();
+
+        mBT_history = (Button) findViewById(R.id.bt_history);
+        mBT_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), DeviceHistoryActivity.class));
+            }
+        });
+
+        button = (ToggleButton) findViewById(R.id.bt_switch);
+        button.setOnToggleChanged(new ToggleButton.OnToggleChanged() {
+            @Override
+            public void onToggle(boolean on) {
+                toggleOn = on;
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "the toggle is" + toggleOn, Toast.LENGTH_SHORT);
+            }
+        });
+
+        mTV_component_id = (TextView) findViewById(R.id.tv_component_id);
+        mTV_component_name = (TextView) findViewById(R.id.tv_component_name);
     }
 }
